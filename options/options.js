29be6c81ea59f2bf5440b16302c0cf540183a4d9
Crowdbotics <team@crowdbotics.com>
2023-08-03 09:19:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bouyela-education-c-42612",
+  url: "https://bouyela-education-c-42612.botics.co",
+  api: "https://bouyela-education-c-42612.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
